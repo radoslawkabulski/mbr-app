e55c314b8f66dd071fafdc8b786fc03d7cefea4f
Radoslaw Kabulski <r.kabulski@gmail.com>
2020-09-10 12:49:56 +0200

    Add client proxy

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import {
+  ClientProxyFactory,
+  Transport,
+  ClientProxy,
+} from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
+  private client: ClientProxy;
+
+  constructor() {
+    this.client = ClientProxyFactory.create({
+      transport: Transport.REDIS,
+      options: {
+        url: 'redis://localhost:6379',
+      }
+    });
+  }
+  
   getHello(): string {
     return 'Hello World!';
   }
